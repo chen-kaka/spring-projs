33efa167473d50555aa9047aae8091c58697772d
kakachan <chen-kaka@163.com>
2017-01-14 23:16:30 +0800

    config-server

---
+++ spring-config-server/src/main/java/com/kakachan/configserver/Application.java
@@ -0,0 +1,16 @@
+package com.kakachan.configserver;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by kakachan on 17/1/14.
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+}
