fd2d06c83e9d5ec938a0c26dc0a7148a8337873b
kakachan <chen-kaka@163.com>
2017-01-15 11:23:51 +0800

    config-server eureka

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 spring projects
+
+服务发现与负载均衡管理
+use Eureka
+project: spring-register
+http://localhost:9500/
+
+配置管理及下发:
+project: spring-config-server
+http://localhost:9700/
+
+服务器端:
+project: spring-provider
+http://localhost:9505/
+
+客户端:
+project: spring-consumer
+http://localhost:9505/
+
--- spring-config-server/src/main/java/com/kakachan/configserver/Application.java
+++ spring-config-server/src/main/java/com/kakachan/configserver/Application.java
@@ -2,15 +2,18 @@ package com.kakachan.configserver;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * Created by kakachan on 17/1/14.
  */
+@EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+
         new SpringApplicationBuilder(Application.class).web(true).run(args);
     }
 }
